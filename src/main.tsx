a77f045b65ffcace47481facb63b0c617dc2825f
Isa Westling <isawestling.blogg@hotmail.com>
2025-02-07 15:54:34 +0100

    Integrerat RouterProvider för att använda routing i applikationen, bytt ut App.tsx mot routerprovider och router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { RouterProvider } from 'react-router-dom' // Importerar RouterProvider för att kunna använda routern
 import './index.css'
-import App from './App.tsx'
+import router from './routing.tsx' // Importerar routern
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    {/* Använder router i RouterProvider */}
+    <RouterProvider router={router} />
   </StrictMode>,
 )
