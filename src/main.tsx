accfe14cda59674277839263892960eda9001cd5
Isa Westling <isawestling.blogg@hotmail.com>
2025-02-07 16:58:13 +0100

    Lagt till AuthProvider för att möjliggöra användning av AuthContext i applikationen

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,14 @@ import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom' // Importerar RouterProvider för att kunna använda routern
 import './index.css'
 import router from './routing.tsx' // Importerar routern
+import { AuthProvider } from './context/AuthContext.tsx' // Importerar AuthProvider för att kunna använda AuthContext
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {/* Använder router i RouterProvider */}
-    <RouterProvider router={router} />
+    {/* Använder AuthProvider för att använda AuthContext */}
+    <AuthProvider>
+      {/* Använder router i RouterProvider */}
+      <RouterProvider router={router} />
+    </AuthProvider>
   </StrictMode>,
 )
